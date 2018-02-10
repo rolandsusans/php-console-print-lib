29499f07fc223f7893228edf7e21017afd7a0157
rolandsusans <rusans@kasko.io>
2018-02-10 22:03:30 +0200

    exceptions

---
+++ src/Exception/Base.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+
+namespace Console\Output\Exception;
+
+
+use InvalidArgumentException;
+
+class Base extends InvalidArgumentException
+{
+
+}
---
+++ src/Exception/Option.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+
+namespace Console\Output\Exception;
+
+
+class Option extends Base
+{
+
+}
